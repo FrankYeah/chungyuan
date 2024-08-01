b3e2770ef4db0910ad84073c110fb535728a5148
葉奕緯 <aaa24295234@gmail.com>
2024-08-01 12:44:18 +0800

    fix north

---
+++ _nuxt/static/1722487406/north-sound/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fchungyuan\u002F_nuxt\u002Fstatic\u002F1722487406",layout:"default",error:a,state:{currentUser:{},tempUser:b,auth:{user:a,loggedIn:false,strategy:b}},serverRendered:true,routePath:"\u002Fnorth-sound",config:{_app:{basePath:"\u002Fchungyuan\u002F",assetsPath:"\u002Fchungyuan\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
